fad0e63ce0e1052fd8922aedbe1c075ed7492f4c
ander <andersonbarreto2001@gmail.com>
2024-09-15 20:37:59 +0200

    rmov table hosts

--- scripts/metrics_table.sql
+++ scripts/metrics_table.sql
@@ -1,13 +1,7 @@
-CREATE TABLE hosts (
-    id SERIAL PRIMARY KEY,
-    host_name VARCHAR(255) UNIQUE NOT NULL
-);
-
 CREATE TABLE system_metrics (
     id SERIAL PRIMARY KEY,
-    host_id INT REFERENCES hosts(id),
+    host_name VARCHAR(255) NOT NULL,
     metric_name VARCHAR(255) NOT NULL,
-    metric_value FLOAT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE (host_id, metric_name, created_at)
+    metric_value VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
